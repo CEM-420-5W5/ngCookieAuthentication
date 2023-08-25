8b4651d6d6a10cf6e680ef01850855460eaf4daa
Mathieu Briau <mathieu.briau@gmail.com>
2023-08-25 10:37:59 -0400

    Création initiale

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngCours2';
+  baseUrl = "https://localhost:7286/api/";
+  accountBaseUrl = this.baseUrl + "Account/";
+
+  constructor(public http: HttpClient, public cookieService: CookieService){}
+
+  async register(){
+    let registerData = {
+      email : "autre2@test.com",
+      password : "Passw0rd!",
+      passwordConfirm : "Passw0rd!",
+    }
+    let result = await lastValueFrom(this.http.post<any>(this.accountBaseUrl + 'Register', registerData));
+    console.log(result);
+  }
+
+  async connect(){
+    let registerData = {
+      username : "autre2@test.com",
+      password : "Passw0rd!"
+    }
+    let result = await lastValueFrom(this.http.post<any>(this.accountBaseUrl + 'Login', registerData));
+    console.log(result);
+  }
+
+  async request(){
+    let result = await lastValueFrom(this.http.get<any>(this.accountBaseUrl + 'Data'));
+    console.log(result);
+  }
+
+  async logout(){
+    let result = await lastValueFrom(this.http.get<any>(this.accountBaseUrl + 'Logout'));
+    console.log(result);
+  }
+}
